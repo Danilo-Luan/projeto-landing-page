ec3286e3c284865f3ebcad52983c3f26a05b2e50
Danilo Luan <estudosteologicoluan@gmail.com>
2025-10-30 13:33:58 -0300

    Versão Inicial

---
+++ index.js
@@ -0,0 +1,22 @@
+var SetaEsquerda = window.document.getElementById("seta-esquerda")
+var Leonardo = window.document.getElementById("Leonardo");
+var Bruna = window.document.getElementById("Bruna");
+var Samantha = window.document.getElementById("Samantha");
+var SetaDireita = window.document.getElementById("seta-direita");
+
+
+function RolarParaDireita() {
+    Leonardo.style.display = "none";
+    Bruna.style.display = "flex";
+    SetaEsquerda.style.display = "flex"; "margin-top:4px";
+    SetaDireita.style.display = "none";
+
+}
+
+function RolarParaEsquerda() {
+Leonardo.style.display = "flex";
+Bruna.style.display = "none"
+SetaEsquerda.style.display = "none"
+SetaDireita.style.display = "flex"
+
+}
